b101fbaf3a9132b106f0ea50886453c65b08a124
Kevin Breslin <kovuthehusky@gmail.com>
2013-03-15 18:06:30 -0400

    Work on quicksort reveals bug with updating for non in-place algorithms, requires rewrite

--- src/SortVisualization.java
+++ src/SortVisualization.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.concurrent.Semaphore;
 
 import javax.sound.sampled.AudioFormat;
@@ -9,9 +10,11 @@ import processing.core.*;
 
 public class SortVisualization extends PApplet {
 	
+	private boolean playBoth = true;
+	
 	private final int SLEEP = 1;
 	
-	private final int ARRAY_LENGTH = 10;
+	private final int ARRAY_LENGTH = 50;
 	private final int WIDTH = 1000;
 	private final int HEIGHT = 1000;
 	private final int MIN = 100;
@@ -112,19 +115,30 @@ public class SortVisualization extends PApplet {
 			
 		}
 		
+		if (!focused) {
+			background(255);
+			fill(0);
+			textSize(72);
+			textAlign(CENTER, CENTER);
+			text("Click to start...", 0, 0, WIDTH, HEIGHT);
+		}
+		
 		textSize(16);
+		textAlign(TOP, LEFT);
 		fill(0);
 		
 		if (running) {
 			endTime = System.currentTimeMillis() - startTime;
 		}
-		text("time: " + ((double)endTime / 1000), 5, 16);
+		text("time: " + ((double)endTime / 1000), 5, 0, WIDTH, 75);
 		long millisSinceLast = System.currentTimeMillis() - secondCount;
 		secondCount = System.currentTimeMillis();
 		if (secondCount % 10 == 0) {
 			fps = 1000 / millisSinceLast;
 		}
-		text("fps: " + (int)fps, 5, 32);
+		text("fps: " + (int)fps, 5, 18, WIDTH, 75);
+		text("sorts: (s)election (i)nsertion (m)erge (b)ogo (w)rong", 5, 36, WIDTH, 75);
+		text("misc: (n)ew (f)lip (c)heck", 5, 54, WIDTH, 75);
 		
 		semaphore.drainPermits();
 		semaphore.release();
@@ -139,18 +153,36 @@ public class SortVisualization extends PApplet {
 		if (running)
 			return;
 		running = true;
-		if (key == 'i')
-			thread("insertionSort");
-		else if (key == 'm')
-			thread("rMergeSort");
-		else if (key == 'c')
-			thread("isSorted");
-		else if (key == 'n')
-			randomize();
-		else if (key == 'f')
-			flip();
-		else {
-			running = false;
+		switch(key) {
+			case 's':
+				thread("selectionSort");
+				break;
+			case 'i':
+				thread("insertionSort");
+				break;
+			case 'm':
+				thread("rMergeSort");
+				break;
+			case 'b':
+				thread("bogoSort");
+				break;
+			case 'w':
+				thread("mergeSort");
+				break;
+			case 'q':
+				thread("quickSort");
+				break;
+			case 'c':
+				thread("isSorted");
+				break;
+			case 'n':
+				randomize();
+				break;
+			case 'f':
+				flip();
+				break;
+			default:
+				running = false;
 		}
 	}
 	
@@ -180,7 +212,7 @@ public class SortVisualization extends PApplet {
 				play(array[cur4], array[cur5], SOUND_LENGTH);
 				//play(array[cur1], 1);
 				if (array[cur4] < array[cur5])
-					play(1, 100, 1000);
+					play(1, MIN, SOUND_LENGTH * 10);
 			}
 			try {
 				semaphore.acquire();
@@ -198,14 +230,62 @@ public class SortVisualization extends PApplet {
 		return true;
 	}
 	
+	public boolean isSortedNoPause() {
+		for (int i = 1; i < array.length; ++i) {
+			cur4 = i;
+			cur5 = i - 1;
+			if (cur4 > 0) {
+//				se.play(se.generateSineWavefreq(array[cur2],1));
+//				se.play(se.generateSineWavefreq(array[cur1],1));
+				play(array[cur4], array[cur5], SOUND_LENGTH);
+				//play(array[cur1], 1);
+				if (array[cur4] < array[cur5])
+					play(1, MIN, SOUND_LENGTH * 10);
+			}
+			try {
+				semaphore.acquire();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			if (array[i] < array[i - 1]) {
+				cur4 = cur5 = -1;
+				return false;
+			}
+		}
+		return true;
+	}
+	
 	private void play(int freq, int freq2, int seconds) {
-		byte[] array = this.generateSineWavefreq(freq, freq2, seconds);
+		
 		int length = sampleRate * array.length / 1000;
-        line.write(array, 0, array.length);
+		if (playBoth) {
+			byte[] array = this.generateSineWavefreq(0, freq2, seconds);
+			byte[] array2 = this.generateSineWavefreq(freq, 0, seconds);
+	        line.write(array, 0, array.length);
+	        line.write(array2, 0, array2.length);
+		} else {
+			byte[] array = this.generateSineWavefreq(freq, freq2, seconds);
+			line.write(array, 0, array.length);
+		}
         line.drain();
 	}
 	
-	public void insertionSort() {
+	public void bogoSort() {
+		startTime = System.currentTimeMillis();
+		while (!isSortedNoPause()) {
+			for (int i = 0; i < array.length; ++i) {
+				swap(i, (int)random(0, array.length));
+				try {
+					semaphore.acquire();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		isSorted();
+	}
+	
+	public void selectionSort() {
 		startTime = System.currentTimeMillis();
 		if (array.length <= 1)
 			return;
@@ -233,6 +313,34 @@ public class SortVisualization extends PApplet {
 				swap(i, min);
 		}
 		cur1 = cur2 = cur3 = -1;
+		isSorted();
+	}
+	
+	public void insertionSort() {
+		startTime = System.currentTimeMillis();
+		if (array.length <= 1)
+			return;
+		for (int i = 0; i < array.length; ++i) {
+			int valueToInsert = array[i];
+			int holePos = i;
+			while (holePos > 0 && valueToInsert < array[holePos - 1]) {
+				
+				//cur2 = -1;
+				cur1 = holePos - 1;
+				if (cur1 > 0)
+					play(valueToInsert, array[cur1], SOUND_LENGTH);
+				try {
+					semaphore.acquire();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				
+				array[holePos] = array[holePos - 1];
+				holePos -= 1;
+			}
+			array[holePos] = valueToInsert;
+		}
+		cur1 = cur2 = -1;
 		isSorted();
 	}
 	
@@ -293,6 +401,67 @@ public class SortVisualization extends PApplet {
 	    for (int i1 = 0; i1 < merge.length; ++i1) {
 	    	array[start++] = merge[i1];
 	    }
+	}
+	
+	public void quickSort() {
+		int[] sorted = quickSort(array);
+		array = sorted;
+	}
+	
+	public int[] quickSort(int[] arr) {
+		if (arr.length < 2)
+			return arr;
+		int pivot = (int)random(0, arr.length - 1);
+		ArrayList<Integer> less = new ArrayList<Integer>();
+		ArrayList<Integer> more = new ArrayList<Integer>();
+		for (int i = 0; i < arr.length; ++i) {
+			
+			if (i == pivot)
+				continue;
+			
+			cur1 = i;
+			cur2 = pivot;
+			if (cur1 > -1)
+				play(array[cur2], array[cur1], SOUND_LENGTH);
+			try {
+				semaphore.acquire();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			if (arr[i] <= arr[pivot])
+				less.add(arr[i]);
+			else
+				more.add(arr[i]);
+		}
+		int[] lessA = new int[less.size()];
+		for (int i = 0; i < less.size(); ++i) {
+			lessA[i] = less.get(i);
+		}
+		int[] moreA = new int[more.size()];
+		for (int i = 0; i < more.size(); ++i) {
+			moreA[i] = more.get(i);
+		}
+		
+		int[] toInsert = concatenate(lessA, arr[pivot], moreA);
+		int pos = 0;
+		for (int i = 0; i < toInsert.length; i++) {
+			array[pos++] = toInsert[i];
+		}
+		
+		return concatenate(quickSort(lessA), arr[pivot], quickSort(moreA));
+	}
+	
+	public int[] concatenate(int[] a, int b, int[] c) {
+		int len = a.length + 1 + c.length;
+		int[] r = new int[len];
+		int pos = 0;
+		for (int i = 0; i < a.length; ++i)
+			r[pos++] = a[i];
+		r[pos++] = b;
+		for (int i = 0; i < c.length; ++i)
+			r[pos++] = c[i];
+		return r;
 	}
 	
 }
